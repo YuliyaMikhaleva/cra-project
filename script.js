c52e7ffca599fe32e4358814d474242b19f488af
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-09-20 00:01:08 +0500

    Создан класс товар GoodsItem со свойствами img, title, price
    Создан класс список товаов GoodsList со свойством goods(пустой массив товаров) и методом заполнения этих товаров и способом отрисовки
    
    Создан класс корзины Basket - со свойством basketProducts (пустой массив товаров корзины) и возможными методами корзины: отрисовать новый продукт, посчитать сумму товаров для оплаты, увеличение количества повторного товара, подсчет суммы за вид товара за указанное количество, очистить корзины

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
 "use strict";
-//Массив со списком товаров. Для каждого товара определим название и цену:
-const goods = [
-    { img: "img/платье.png",title: 'Платье', price: 1900 },
-    { img: "img/штаны.png", title: 'Штаны', price: 1500 },
-    { img: "img/юбка.png", title: 'Юбка', price: 1000 },
-    { img: "img/флиска.png", title: 'Флисовая кофта', price: 2000 },
-    { img: "img/футболка.png", title: 'Футболка', price: 600 },
-    { img: "img/шорты.png", title: 'Шорты', price: 800 },
-    { img: "img/кепка.png", title: 'Кепка', price: 500 },
-    { img: "img/кроссовки.png" , title: 'Кроссовки', price: 3200 },
-
-];
-
-//Функция, которая будет возвращать разметку для конкретного товара, подставляя его название и цену.
-
-const renderGoodsItem = (img="img/standart.jpg", title="товар на изображении", price) => {
-    return `
+//Создадим класс GoodsItem для товара, чтобы на его основе создавать другие однотипные товары
+class GoodsItem {
+    constructor(img, title, price) {
+        this.img = img;
+        this.title = title;
+        this.price = price;
+    }
+    render(){
+        return `
         <div class="catalog_cart">
-            <img class="catalog_cart_img" src=${img} width="250" height="270" alt="photo">
-            <h3 class="catalog_cart_title">${title}</h3>
-            <span class="catalog_cart_price"><span>${price}</span>рублей</span>
+            <img class="catalog_cart_img" src=${this.img} width="250" height="270" alt="photo">
+            <h3 class="catalog_cart_title">${this.title}</h3>
+            <span class="catalog_cart_price"><span>${this.price}</span>рублей</span>
             <button class="catalog_cart_button">Добавить в корзину</button>
         </div>
-  `;
-};
-
-//Функция, которая будет собирать все товары в один и записывать его в контейнер .catalog:
-const renderGoodsList = list => {
-    document.querySelector('.catalog').innerHTML = list.map(item => renderGoodsItem(item.img, item.title, item.price)).join("");//а затем вставляться в блок с классом main-products
+        `
+    }
 }
 
-renderGoodsList(goods);//так мы вызываем вышестоящую функцию
+//теперь создадим второй класс GoodsList. В качестве свойства ему добавим массив со списком товаров,
+// изначально сделаем его пустым
+//теперь создадидим второй класс GoodsList. В качестве свойства ему добавим массив со списком товаров,
+// изначально сделаем его пустым
+class GoodsList {
+    constructor() {
+        this.goods = [];//пустой массив товаров
+    }
+    fetchGoods() {//метод для заполнения этого списка товаров
+        this.goods = [
+            {img: "img/платье.png", title: 'Платье', price: 1900},
+            {img: "img/штаны.png", title: 'Штаны', price: 1500},
+            {img: "img/юбка.png", title: 'Юбка', price: 1000},
+            {img: "img/флиска.png", title: 'Флисовая кофта', price: 2000},
+            {img: "img/футболка.png", title: 'Футболка', price: 600},
+            {img: "img/шорты.png", title: 'Шорты', price: 800},
+            {img: "img/кепка.png", title: 'Кепка', price: 500},
+            {img: "img/кроссовки.png", title: 'Кроссовки', price: 3200},
+        ];
+    }
+
+    //Метод для вывода списка товаров на страницу. Для каждого элемента массива goods будем создавать экземпляр класса GoodsItem и запрашивать его разметку
+    render() {
+        let listHtml = " ";//создаем пустую строку
+        this.goods.forEach(good => {//перебираем товары и для каждого товара создаем на оснвое класса GoodsItem создаем goodItem
+            const goodItem = new GoodsItem(good.img, good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.catalog').innerHTML = listHtml;
+    }
+}
+
+const list = new GoodsList(); //создаем список на основе класса GoodsList
+list.fetchGoods(); //вызываем метод заполнения товаров
+list.render();//отрисовываем
+
+//Добавим пустой класс для корзины товаров
+class Basket {
+    constructor() {
+        this.BasketProducts = [];
+    }
+    renderNewProduct(){}//отрисовывание в корзине нового продукта в количестве 1 шт
+    calculateSum(){}//Подсчитать итоговую сумму выбранных товаров для оплаты
+    increaseProductsCount(){}//если данный товар уже добавлен, увеличить количество повторного товара
+    recalculateSumOfProducts(){}//подсчет суммы за вид товара за указанное количество
+    clearBasket(){}//очистить корзину
+}
+
+//Добавим пустой класс для элемента корзины товаров
+class BasketProduct{
+    constructor() {
+
+    }
+    render(){}  //отобразить элемент на странице
+}
+
+let buttonOpenBusket = document.querySelector('.header_menu_el_basket');
+let basket = document.querySelector('.basket');
+
+buttonOpenBusket.addEventListener('click', () => {
+    basket.classList.toggle('hidden')
+})
